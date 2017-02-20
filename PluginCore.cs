03dcc38b849b736940422fa02009f69a99b4a5a0
Bryce Mecum <petridish@gmail.com>
2017-02-19 22:53:51 -0900

    Clean up imports and clean old copyright

--- PluginCore.cs
+++ PluginCore.cs
@@ -1,31 +1,3 @@
-///////////////////////////////////////////////////////////////////////////////
-//File: PluginCore.cs
-//
-//Description: An example plugin using the VVS MetaViewWrappers. When VVS is
-//  enabled, the plugin's view appears under the VVS bar. Otherwise, it appears
-//  in the regular Decal bar.
-//
-//This file is Copyright (c) 2009 VirindiPlugins
-//
-//Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
-///////////////////////////////////////////////////////////////////////////////
-
 using System;
 using System.Reflection;
 using System.IO;
@@ -97,7 +69,7 @@ namespace PhatACUtil
 
         public static void LogError(Exception ex) {
             try {
-                System.IO.StreamWriter sw = new System.IO.StreamWriter(LogPath, true);
+                StreamWriter sw = new StreamWriter(log_path, true);
 
                 sw.WriteLine("============================================================================");
                 sw.WriteLine(DateTime.Now.ToString());
