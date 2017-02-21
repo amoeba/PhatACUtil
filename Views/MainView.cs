0c3fc38c30a18f8ff9069654d4f9d85fe5dbf548
Bryce Mecum <petridish@gmail.com>
2017-02-20 20:19:15 -0900

    Add Clear Last Assessed buttons

--- Views/MainView.cs
+++ Views/MainView.cs
@@ -15,6 +15,7 @@ namespace PhatACUtil
         static ITextBox txtSpawnModelSearchText;
         static IList lstSpawnModelSearchList;
         static IButton btnSpawnModelClearSpawns;
+        static IButton btnSpawnModelClearLastAssessed;
 
         // Spawn Tool
         static ITextBox txtSpawnToolCommand;
@@ -24,6 +25,7 @@ namespace PhatACUtil
         static IButton btnSpawnToolSpawnMinus;
         static IButton btnSpawnToolSpawnPlus;
         static IButton btnSpawnToolClearSpawns;
+        static IButton btnSpawnToolClearLastAssessed;
 
 
 
@@ -34,19 +36,20 @@ namespace PhatACUtil
             View = ViewSystemSelector.CreateViewResource(PluginCore.MyHost, "PhatACUtil.Views.MainView.xml");
 
             // Spawn
-            // Spawn:model
+            // Spawn Model
             txtSpawnModelSearchText = (ITextBox)View["txtSpawnModelSearchText"]; 
             lstSpawnModelSearchList = (IList)View["lstSpawnModelSearchList"];
-            btnSpawnModelClearSpawns = (IButton)View["btnSpawnModelClearSpawns"];
             txtSpawnModelSearchText.Change += txtSpawnModelSearchText_Change;
             lstSpawnModelSearchList.Selected += lstSpawnModelSearchList_Selected;
+            btnSpawnModelClearSpawns = (IButton)View["btnSpawnModelClearSpawns"];
             btnSpawnModelClearSpawns.Hit += btnSpawnModelClearSpawns_Hit;
-            
+            btnSpawnModelClearLastAssessed = (IButton)View["btnSpawnModelClearLastAssessed"];
+            btnSpawnModelClearLastAssessed.Hit += btnSpawnModelClearLastAssessed_Hit;
+
             // Spawn Tool
             txtSpawnToolCommand = (ITextBox)View["txtSpawnToolCommand"];
             txtSpawnToolValue = (ITextBox)View["txtSpawnToolValue"];
             btnSpawnToolSpawn = (IButton)View["btnSpawnToolSpawn"];
-
             btnSpawnToolSpawn.Hit += btnSpawnToolSpawn_Hit;
             btnSpawnToolSpawnTenX = (IButton)View["btnSpawnToolSpawnTenX"];
             btnSpawnToolSpawnTenX.Hit += btnSpawnToolSpawnTenX_Hit;
@@ -56,29 +59,39 @@ namespace PhatACUtil
             btnSpawnToolSpawnPlus.Hit += btnSpawnToolSpawnPlus_Hit;
             btnSpawnToolClearSpawns = (IButton)View["btnSpawnToolClearSpawns"];
             btnSpawnToolClearSpawns.Hit += btnSpawnToolClearSpawns_Hit;
+            btnSpawnToolClearLastAssessed = (IButton)View["btnSpawnToolClearLastAssessed"];
+            btnSpawnToolClearLastAssessed.Hit += btnSpawnToolClearLastAssessed_Hit;
 
             addAll();
         }
 
         public static void ViewDestroy()
         {
+            // Events
             txtSpawnModelSearchText.Change -= txtSpawnModelSearchText_Change;
             lstSpawnModelSearchList.Selected -= lstSpawnModelSearchList_Selected;
             btnSpawnModelClearSpawns.Hit -= btnSpawnModelClearSpawns_Hit;
+
             btnSpawnToolSpawn.Hit -= btnSpawnToolSpawn_Hit;
             btnSpawnToolSpawnTenX.Hit -= btnSpawnToolSpawnTenX_Hit;
             btnSpawnToolSpawnMinus.Hit -= btnSpawnToolSpawnMinus_Hit;
             btnSpawnToolSpawnPlus.Hit -= btnSpawnToolSpawnPlus_Hit;
             btnSpawnToolClearSpawns.Hit -= btnSpawnToolClearSpawns_Hit;
+            btnSpawnToolClearLastAssessed.Hit -= btnSpawnToolClearLastAssessed_Hit;
 
+            // Controls
             txtSpawnModelSearchText = null;
             lstSpawnModelSearchList = null;
             btnSpawnModelClearSpawns = null;
+            btnSpawnModelClearLastAssessed = null;
+
+            btnSpawnToolClearSpawns = null;
             btnSpawnToolSpawn = null;
             btnSpawnToolSpawnTenX = null;
             btnSpawnToolSpawnMinus = null;
             btnSpawnToolSpawnPlus = null;
             btnSpawnToolClearSpawns = null;
+            btnSpawnToolClearLastAssessed = null;
 
             View.Dispose();
         }
@@ -169,6 +182,11 @@ namespace PhatACUtil
             }
         }
 
+        private static void btnSpawnModelClearLastAssessed_Hit(object sender, EventArgs e)
+        {
+            ChatCommand("removethis");
+        }
+
         private static void btnSpawnToolSpawn_Hit(object sender, EventArgs e)
         {
             StringBuilder sb = new StringBuilder();
@@ -236,6 +254,11 @@ namespace PhatACUtil
             {
                 PluginCore.LogError(ex);
             }
+        }
+
+        private static void btnSpawnToolClearLastAssessed_Hit(object sender, EventArgs e)
+        {
+            ChatCommand("removethis");
         }
 
         static void addAll()
